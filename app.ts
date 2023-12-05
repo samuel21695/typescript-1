2db6f1b4be92fc4802ccc1005502a77f2870adf7
samuel21695 <samuel21695@gmail.com>
2023-12-05 23:52:40 +0900

    function createComponet-2

--- app.ts
+++ app.ts
@@ -16,5 +16,14 @@ function createComponent(element: string, props: Props, children?: string[]): st
     const result = objectValues.join(' ');
   };
 
+  for (const [key, value] of Object.entries(props)) {
+    let attributeString;
+    if (key === 'style' && typeof (value) === 'object') {
+      attributeString = ` style="${styleToString(value)}"`;
+    } else {
+      attributeString = ` ${key}="${value}"`;
+    }
+    tagParts.push(attributeString);
+  }
   
 }
