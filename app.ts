bdb02256481583a7142329cfb2bcc3e339df178d
samuel21695 <samuel21695@gmail.com>
2023-12-05 23:42:40 +0900

    createComponet function-1

--- app.ts
+++ app.ts
@@ -6,3 +6,15 @@ interface Props {
 
 // 인터페이스의 사전적인 정의는 '약속'이다.
 // typescript의 interface는 약속이다.
+
+function createComponent(element: string, props: Props, children?: string[]): string {
+  let tagParts = [`<${element}`];
+
+  const styleToString = (style: Partial<CSSStyleDeclaration>): string => {
+    const entries = Object.entries(style);
+    const objectValues = entries.map(([key, value]) => `${key}: ${value};`);
+    const result = objectValues.join(' ');
+  };
+
+  
+}
